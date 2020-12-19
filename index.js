4c2139561eddceef7b33889957de4b68a8d4c599
7kasper <kasper77077@gmail.com>
2020-12-19 12:34:47 +0100

    Add chat test :-)

--- index.js
+++ index.js
@@ -11,12 +11,23 @@ new RestNio((router, rnio) => {
 
     router.ws('/join', {
         params: {
-            room: {
-                type: 'string',
-                required: true
-            }
+            room: rnio.params.forcedString
         },
-        
+        func: (params, client) => {
+            client.props.room = params.room;
+            client.subscribe(params.room);
+            return `Subscribed to ${params.room}`;
+        }
+    });
+
+    router.ws('/chat', {
+        params: {
+            msg: rnio.params.forcedString
+        },
+        func: (params, client) => {
+            if (!client.props.room) throw [403, 'Client is not in a room!'];
+            rnio.subs(client.props.room).obj({type: 'chat', msg: params.msg});
+        }
     });
 
 }, {
